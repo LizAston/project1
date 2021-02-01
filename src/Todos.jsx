dd0914026fba417e95fd9f60f63813bfc1444362
Liz Aston <elizabethjaneaston@gmail.com>
2021-02-01 18:18:29 +0000

    Project1 video #11

---
+++ src/Todos.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { CircularProgress } from "@material-ui/core";
+import TodoCard from "./TodoCard"
+
+const Todos = () => {
+
+    const [todos, setTodos] = useState();
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/todos`).then((res) => {
+          const responseTodos = res.data;
+          setTodos(responseTodos);
+      });
+    }, []);
+
+    return (
+      <>{todos ? 
+      (
+      <div style = {{display: "flex", flexWrap: "wrap"}}>
+          {todos.slice(0, 10).map((todo) => (
+            <TodoCard todo={todo} />
+          ))}
+      </div>
+      )
+      : <CircularProgress />}
+      </>
+    );
+};
+
+export default Todos;
